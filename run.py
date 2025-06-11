568aaa322b67f92275e9e01be58ce5cbe37d7145
Nsl-14 <sclanonicola@gmail.com>
2025-06-11 17:54:17 +0100

    Update run.py

--- run.py
+++ run.py
@@ -5,14 +5,14 @@ import re
 import string
 
 # Initialize the main FastAPI application
-main_app = FastAPI()
+app = FastAPI()
 
 # Manually add only non-static routes from mediaflow_app
 for route in mediaflow_app.routes:
     if route.path != "/":  # Exclude the static file path
-        main_app.router.routes.append(route)
+        app.router.routes.append(route)
 
 # Run the main app
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(main_app, host="0.0.0.0", port=8888)
+    uvicorn.run(app, host="0.0.0.0", port=8888)
